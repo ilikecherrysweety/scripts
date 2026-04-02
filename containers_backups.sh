96d478f058f16c8d43a0cd5b6c9949c278456471
ilikecherrysweety <spirik360@gmail.com>
2026-04-02 17:34:49 +0300

    added containers_backups.sh

---
+++ containers_backups.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+DATE=$(date -I)
+MAINPATH=/nfs/containers
+BCKPPATH=/nfs/containers_backups/containers_$DATE
+LOG=/var/log/sql_backup.log
+
+log() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $LOG
+}
+
+error_exit() {
+    log "ERROR: $1"
+    exit 1
+}
+
+log "Starting backup | $DATE"
+
+# Copy
+log "Copying..."
+rsync -a --info=progress2 $MAINPATH $BCKPPATH || error_exit "Failed to copy!"
+log "Copying is complete."
+
+# Archive
+log "Archiving..."
+tar -c $BCKPPATH | pv | gzip > $BCKPPATH.tar.gz || error_exit "Failed to archive!"
+rm -rf $BCKPPATH
+log "Archiving is complete."
+
+log "Backup is READY | $DATE"
